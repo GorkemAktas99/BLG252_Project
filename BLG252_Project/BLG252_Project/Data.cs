334ec736d96fbb3b88c81d199a666451d65bf4ee
GorkemAktas99 <72526481+GorkemAktas99@users.noreply.github.com>
2021-03-25 02:49:36 +0300

    Add files via upload

---
+++ BLG252_Project/BLG252_Project/Data.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace BLG252_Project
+{
+    public class Data
+    {
+        private string[] allLines = System.IO.File.ReadAllLines(@"C:\Users\90535\Desktop\FearStudyData.csv");
+        private string[] fear = new string[80];
+        private string[] greatest = new string[80];
+        private string[] impact = new string[80];
+        private string[] past = new string[80];
+        private string[] encounter = new string[80];
+        private string[] overcome = new string[80];
+        private string[] embarrassed = new string[80];
+        private int i = 0;
+        private double successedCounter , dataCounter = 0;
+        private double successedProportion;
+
+        public void Reading()
+        {
+            var query = from line in allLines
+                let data = line.Split(',') //Ayraç;
+                select new
+                {
+                    Fear = data[0],
+                    Greatest = data[1],
+                    Impact = data[2],
+                    Past = data[3],
+                    Encounter = data[4],
+                    Overcome = data[5],
+                    Embarrassed = data[6],
+                };
+            
+            foreach (var item in query)
+            {
+                fear[i] = item.Fear;
+                greatest[i] = item.Greatest;
+                impact[i] = item.Impact;
+                past[i] = item.Past;
+                encounter[i] = item.Encounter;
+                overcome[i] = item.Overcome;
+                embarrassed[i] = item.Embarrassed;
+                i++;
+            }
+        }
+
+        public void ShowData()
+        {
+            for (i = 0; i < 80; i++)
+            {
+                
+                Console.WriteLine($"{i}.Line - {fear[i]} - {greatest[i]} - {impact[i]} - {past[i]} - {encounter[i]} - {overcome[i]} - {embarrassed[i]}");
+            }
+        }
+
+        public void CollectFear(string keyWord)
+        {
+            for (i = 0; i < 80; i++)
+            {
+                if (keyWord == fear[i])
+                {
+                    Console.WriteLine($"{i}.Line - {fear[i]} - {greatest[i]} - {impact[i]} - {past[i]} - {encounter[i]} - {overcome[i]} - {embarrassed[i]}");
+                }
+            }
+        }
+
+        public double OvercomingFear(string keyWord)
+        {
+            successedCounter = 0;
+            dataCounter = 0;
+            for (i = 0; i < 80; i++)
+            {
+                if (keyWord == fear[i])
+                {
+                    if (overcome[i] == "Yes")
+                    {
+                        successedCounter++;
+                        dataCounter++;
+                    }
+                    else if (overcome[i] == "No")
+                    {
+                        dataCounter++;
+                    }
+                }
+            }
+
+            successedProportion = Convert.ToDouble(successedCounter / dataCounter);
+            return successedProportion*100;
+        }
+        
+    }
+}
---
+++ BLG252_Project/BLG252_Project/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+
+namespace BLG252_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int select = 1;
+            string _fear;
+            Data data = new Data();
+            data.Reading();
+            while (select != 0)
+            {
+                Console.WriteLine("Please select a process");
+                Console.WriteLine("---------------------------");
+                Console.WriteLine("1-Show All Table");
+                Console.WriteLine("2-Show Filtered Fear");
+                Console.WriteLine("3-Show Success Proportion of a Fear");
+                Console.WriteLine("0-Quit");
+                Console.WriteLine("---------------------------");
+                select = Convert.ToInt32(Console.ReadLine());
+                switch (select)
+                {
+                    case 1:
+                        data.ShowData();
+                        Console.WriteLine("Please enter anything for continue");
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.WriteLine("Please enter the fear");
+                        _fear = Console.ReadLine();
+                        data.CollectFear(_fear);
+                        Console.WriteLine("Please enter anything for continue");
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("Please enter the fear");
+                        _fear = Console.ReadLine();
+                        Console.WriteLine(data.OvercomingFear(_fear) + " %");
+                        Console.WriteLine("Please enter anything for continue");
+                        Console.ReadLine();
+                        break;
+                    case 0: Console.WriteLine("Please Wait, Closing...");
+                        break;
+                    default:
+                        Console.Error.WriteLine("Wrong Process");
+                        Console.WriteLine("Please enter anything for continue");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
+    }
+}
